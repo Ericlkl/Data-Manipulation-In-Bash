46ca6ed6e2d6d4607f0c9aff19262f20a704d229
Eric Lee <Raidenlkl@gmail.com>
2019-08-11 12:46:57 +1000

    Bash question 1 completed, Sqlite question 1 OFS fix

---
+++ bq1.sh
@@ -0,0 +1,16 @@
+echo "Bash Question 1: How many movies has a director made \n"
+
+# Steps
+# 1: Get Director column
+# 2: Remove the first director text label
+# 3: sort the result
+# 4: unique command to remove duplicate names and count how many times
+#    it appears
+# 5: add ',' to seperate the columns 
+# 6: use awk to change the position
+# 7: save it to file
+
+echo "Generating Result.... \n"
+cut -f 6 -d, data/biopics.csv | grep -v director | sort | uniq -c | sed -e 's/^ *//;s/ /,/' | awk 'BEGIN { OFS = ","; FS="," } {print $2, $1}' > output/ba1.txt
+
+echo "Result Generated ! Please check output/ba1.txt"
--- sq1.sh
+++ sq1.sh
@@ -1,1 +1,11 @@
-sqlite3 data/biopics.sqlite 'SELECT director, COUNT(*) FROM biopics GROUP BY director' > sa1.txt
+echo "SQL Question 1: How many movies has a director made \n"
+
+echo "----------------------------------------------"
+echo "Converting biopics.csv to biopics.sqlite ..."
+python3 ./data/csv2sqlite.py --table-name biopics --input ./data/biopics.csv --output ./data/biopics.sqlite
+echo "Convert Completed! \n"
+
+echo "---------------------------------------------- "
+echo "Generating Report as sa1.txt ..."
+sqlite3 data/biopics.sqlite -separator ','  ' SELECT director, COUNT(*) FROM biopics GROUP BY director' > output/sa1.txt
+echo "sa1.txt file generated complete! Please check output/sa1.txt"
