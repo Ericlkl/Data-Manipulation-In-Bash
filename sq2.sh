38e1f8fa6b3c0d69cdbf8e7fd73cc8045c3c7382
Eric Lee <Raidenlkl@gmail.com>
2019-08-12 11:52:09 +1000

    Table Fix

--- sq2.sh
+++ sq2.sh
@@ -8,18 +8,20 @@ echo "Convert Completed! \n"
 echo "---------------------------------------------- "
 echo "Generating Report as sa2.html ..."
 
-# Generate HTML Head
-echo '<html lang="en"><head></head><body>' > sa2.html
+# Generate HTML Body
+echo '<html lang="en"><head></head><body><table><tr><th>Gender</th><th>Total Amount</th></tr>' > output/sa2.html
 
 # Male Query Cmd
 # Step 1: Query Distinct Movie title which subject sex = Male
 # Step 2: SUM the box office by subject sex
-sqlite3 data/biopics.sqlite ' SELECT subject_sex as gender, SUM(box_office) FROM (SELECT DISTINCT title, box_office, subject_sex from biopics WHERE subject_sex = "Male" ) GROUP BY gender' | sed 's/^/<tr><td>/g'  |sed 's/|/<\/td><td>/g' | sed 's/$/<\/td><\/tr>/g' >> sa2.html
+sqlite3 data/biopics.sqlite ' SELECT subject_sex as gender, SUM(box_office) FROM (SELECT DISTINCT title, box_office, subject_sex from biopics WHERE subject_sex = "Male" ) GROUP BY gender' | sed 's/^/<tr><td>/g'  |sed 's/|/<\/td><td>$/g' | sed 's/$/<\/td><\/tr>/g' >> output/sa2.html
 
 # Female Query Cmd
 # Step 1: Query Distinct Movie title which subject sex = Female
 # Step 2: SUM the box office by subject sex
-sqlite3 data/biopics.sqlite ' SELECT subject_sex as gender, SUM(box_office) FROM (SELECT DISTINCT title, box_office, subject_sex from biopics WHERE subject_sex = "Female" ) GROUP BY gender' | sed 's/^/<tr><td>/g'  |sed 's/|/<\/td><td>/g' | sed 's/$/<\/td><\/tr>/g'>> sa2.html
+sqlite3 data/biopics.sqlite ' SELECT subject_sex as gender, SUM(box_office) FROM (SELECT DISTINCT title, box_office, subject_sex from biopics WHERE subject_sex = "Female" ) GROUP BY gender' | sed 's/^/<tr><td>/g'  |sed 's/|/<\/td><td>$/g' | sed 's/$/<\/td><\/tr>/g'>> output/sa2.html
 
-echo '</body></html>' >> sa2.html
+# Generate HTML END Tag
+echo '</table></body></html>' >> output/sa2.html
+
 echo "sa2.html file generated complete! Please check output/sa2.html"
