f6dffe2973d29b10856b62252a7d636e90356032
Eric Lee <Raidenlkl@gmail.com>
2019-08-12 16:49:43 +1000

    command added

--- bq3.sh
+++ bq3.sh
@@ -7,6 +7,18 @@ echo "Generating Report as ba3.html ..."
 echo '<html lang="en"><head></head><body><table><tr><th>Year</th><th>Average Gross</th></tr>' > output/ba3.html
 
 # Query
+
+# Step 1: Select Title, box office, year
+# Step 2: Remove Duplicate record
+# Step 3: Select Only Box Office and year
+# Step 4: Remove all unknown box office record change it to zero
+# Step 5: Remove year_release,box_office label on the top
+# Step 6: Sort 
+# Step 7: Remove Duplicate
+# Step 8: If the record is not 0, Count + 1 / Sum + previous 
+# At the end print the result
+# Step 9: Sort records Reverse
+# Step 10 - 12: Add HTML Table Tag
 cut -f 1,4,5 -d , data/biopics.csv |
 uniq |
 cut -f 2,3 -d , |
