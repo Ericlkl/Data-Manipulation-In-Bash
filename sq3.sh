0bd19324c0eca4ba4c673840725766c0e8816a9b
Eric Lee <Raidenlkl@gmail.com>
2019-08-12 11:55:57 +1000

    SQL Question 3 Table added

--- sq3.sh
+++ sq3.sh
@@ -6,6 +6,15 @@ python3 ./data/csv2sqlite.py --table-name biopics --input ./data/biopics.csv --o
 echo "Convert Completed! \n"
 
 echo "---------------------------------------------- "
+
 echo "Generating Report as sa3.html ..."
-sqlite3 data/biopics.sqlite -separator ','  ' SELECT year_release as Year, SUM(box_office) as total_amount FROM biopics GROUP BY Year' 
-echo "sa1.txt file generated complete! Please check output/sa1.html"
+
+# Generate HTML Body
+echo '<html lang="en"><head></head><body><table><tr><th>Year</th><th>Average Gross</th></tr>' > output/sa3.html
+
+sqlite3 data/biopics.sqlite ' SELECT year_release as Year, SUM(box_office) as total_amount FROM biopics GROUP BY Year' | sed 's/^/<tr><td>/g'  |sed 's/|/<\/td><td>$/g' | sed 's/$/<\/td><\/tr>/g' >> output/sa3.html
+
+# Generate HTML END Tag
+echo '</table></body></html>' >> output/sa2.html
+
+echo "sa3.html file generated complete! Please check output/sa3.html"
