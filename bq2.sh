446d2c6b85df6809e8ae20f006234c10fc725572
Eric Lee <Raidenlkl@gmail.com>
2019-08-15 12:24:31 +1000

    Bash Question 2 improved and completed

--- bq2.sh
+++ bq2.sh
@@ -1,46 +1,40 @@
-echo "Bash Q2: Does gender influence how much a movie earns at the box office ? \n"
+# CAB220 Portfolio 1
+# Developed By: KA LONG LEE 
+# Student ID: N9845097
 
-echo "---------------------------------------------- "
+# File Name: bq2.sh
+# Purpose : Find out Does gender influence how much a movie earns at the box office ?
+# Required: Please enter the file in the parameter one
+#  example: ./clean.sh data/biopics.csv
 echo "Generating Report as ba2.html ..."
 
 # Generate HTML Body
 echo '<html lang="en"><head></head><body><table><tr><th>Gender</th><th>Total Amount</th></tr>' > output/ba2.html
 
-# Male Query
-# STEP 1: Search Only Male Result
-# STEP 2: Get only title, box_office, Male Column
-# STEP 3: GET The unique title (Because some of the movie record is duplicated)
-# STEP 4: Remove Title Column (It is useless for our result)
-# STEP 5: Change all the Unknown box office record to 0 (- to 0)
-# STEP 6: AWK cmd to summarise the result
+# Query Function
+# What it does : Query how much does gender earns at the box office
+genderQueryAndSave(){
+  # STEP 1: Clean biopics.csv data but not save to the file
+  # STEP 2: Search Only Male Result
+  # STEP 3: Get only title, box_office, Gender
+  # STEP 4: GET The unique title (Because some of the movie record is duplicated)
+  # STEP 5: Remove Title Column (It is useless for our result)
+  # STEP 6: AWK cmd to summarise the result
 
-grep Male data/biopics.csv | 
-cut -f 1,5,13 -d , | 
-uniq | 
-cut -f 2,3 -d, | 
-sed 's/-/0/g' | 
-awk ' BEGIN { OFS = ","; FS="," } {a += $1} END{print $2, a}' |
-sed 's/^/<tr><td>/g'|
-sed 's/,/<\/td><td>$/g'|
-sed 's/$/<\/td><\/tr>/g' >> output/ba2.html
+  ./clean.sh $1 |
+  grep $2 |
+  cut -f 1,5,13 -d , |
+  uniq | 
+  cut -f 2,3 -d, | 
+  awk ' BEGIN { OFS = ","; FS="," } {a += $1} END{print $2, a}' |
+  sed 's/^/<tr><td>/g'|
+  sed 's/,/<\/td><td>$/g'|
+  sed 's/$/<\/td><\/tr>/g' >> output/ba2.html
+}
 
-# Female Query
-# STEP 1: Search Only Male Result
-# STEP 2: Get only title, box_office, Female Column
-# STEP 3: GET The unique title (Because some of the movie record is duplicated)
-# STEP 4: Remove Title Column (It is useless for our result)
-# STEP 5: Change all the Unknown box office record to 0 (- to 0)
-# STEP 6: AWK cmd to summarise the result
-
-grep Female data/biopics.csv |
-cut -f 1,5,13 -d , |
-uniq | 
-cut -f 2,3 -d, | 
-sed 's/-/0/g' | 
-awk ' BEGIN { OFS = ","; FS="," } {a += $1} END{print $2, a}' |
-sed 's/^/<tr><td>/g'|
-sed 's/,/<\/td><td>$/g'|
-sed 's/$/<\/td><\/tr>/g' >> output/ba2.html
+# Call Query function Params1 = filename, Params2 = gender
+genderQueryAndSave $1 Male
+genderQueryAndSave $1 Female
 
 # Generate HTML END Tag
 echo '</table></body></html>' >> output/ba2.html
